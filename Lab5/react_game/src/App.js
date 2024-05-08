210fef90ad8fbf7dff50cd112bc6f296bb7dcdab
larry487 <119911822+larry487@users.noreply.github.com>
2024-05-08 16:50:28 +0800

    Add files via upload

---
+++ Lab5/react_game/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Square from './game';
+//import './game.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      < Square />
+    </div>
+  );
+}
+
+export default App;
